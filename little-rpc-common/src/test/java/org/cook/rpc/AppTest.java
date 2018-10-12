653c5d9233da4b320e557a12470edf94c034da0e
zhangying <zhangying@hujiang.com>
2018-10-12 19:15:21 +0800

    update: dubbo pom

--- little-rpc-common/src/test/java/org/cook/rpc/AppTest.java
+++ little-rpc-common/src/test/java/org/cook/rpc/AppTest.java
@@ -1,20 +1,48 @@
 package org.cook.rpc;
 
-import static org.junit.Assert.assertTrue;
-
+import org.cook.rpc.sample.model.clock.DayOfWeek;
+import org.cook.rpc.sample.model.clock.LocalTime;
+import org.cook.rpc.sample.model.clock.LocalTimes;
 import org.junit.Test;
+import sun.misc.HexDumpEncoder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() throws IOException {
+        LocalTimes lts = LocalTimes.newBuilder()
+                .addLocalTime(LocalTime.newBuilder()
+                        .setYear(2018)
+                        .setMonth(11)
+                        .setDayOfMonth(12)
+                        .setHour(12)
+                        .setMinute(12)
+                        .setSecond(12)
+                        .setDayOfWeek(DayOfWeek.FRIDAY)
+                        .build())
+                .build();
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        lts.writeTo(baos);
+
+        HexDumpEncoder encoder = new HexDumpEncoder();
+        System.out.println(encoder.encode(baos.toByteArray()));
+
+        System.out.println(Base64.getEncoder().encodeToString(baos.toByteArray()) );
+        System.out.println("----");
+
+        LocalTimes parsed = LocalTimes.parseFrom(baos.toByteArray());
+        System.out.println(parsed.getLocalTime(0));
+
     }
+
 }
